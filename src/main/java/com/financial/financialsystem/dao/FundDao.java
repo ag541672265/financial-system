61cb2ba2305d5a3922f830e160cb568fe2f2c0de
ykp <1059193212@qq.com>
2019-09-02 17:51:38 +0800

    基金分程序写的差不多了，我布到我的云上了，主程序这边可以调

--- src/main/java/com/financial/financialsystem/dao/FundDao.java
+++ src/main/java/com/financial/financialsystem/dao/FundDao.java
@@ -1,12 +1,11 @@
 package com.financial.financialsystem.dao;
 
-import com.financial.financialsystem.entity.Funddetails;
-import com.financial.financialsystem.entity.Userfund;
+import com.financial.financialsystem.external.fund.Userfund;
 
 import java.util.List;
 
 public interface FundDao {
-    //链接基金程序用的
+    /*//链接基金程序用的
     //将基金的利率（Funddetails）存入自己的数据库
     int getfunddetailme(Funddetails funddetails);
     //将基金个人表（Userfund）的数据存入自己数据库
@@ -14,7 +13,7 @@ public interface FundDao {
 
 
     //这是找出这个用户所有的基金的表，用作资产页面结算
-    List<Userfund> getufl(Integer uid);
+    List<Userfund> getufl(Integer uid);*/
 
 
 
