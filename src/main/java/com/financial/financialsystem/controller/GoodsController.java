f33bd76a1817bb93ce9396e4f273ec19717f5278
ykp <1059193212@qq.com>
2019-09-04 11:38:53 +0800

    给程达的理财商城加上了购买，现在可以购买了。

--- src/main/java/com/financial/financialsystem/controller/GoodsController.java
+++ src/main/java/com/financial/financialsystem/controller/GoodsController.java
@@ -1,12 +1,13 @@
 package com.financial.financialsystem.controller;
 
+import com.financial.financialsystem.entity.Users;
 import com.financial.financialsystem.services.GoodsService;
 import com.financial.financialsystem.entity.Goods;
 import com.financial.financialsystem.services.GoodsService;
 import com.financial.financialsystem.services.UserService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import org.springframework.ui.Model;
@@ -16,6 +17,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -24,21 +27,6 @@ import java.util.List;
 
 @Controller
 public class GoodsController {
-
-    @Resource
-    private GoodsService goodsService;
-
-    @RequestMapping("/dianjigoumai")
-    @ResponseBody
-    public String dianjigoumai(Integer uid, Integer gid,Integer type,double money,double capital,double balance){
-        if(goodsService.zhifumoney(uid,gid,type,money,capital,balance)){
-            System.out.println("xxxxxx");
-            return "<h>ok</h>";
-        }else{
-            return "<h>no</h>";
-        }
-
-    }
 
     @Resource
     private GoodsService service;
@@ -73,11 +61,18 @@ public class GoodsController {
 
     @RequestMapping(value = "/toBuy")
     @ResponseBody
-    public String quer(){
-
-
-
-        return "成大真帅";
+    public String quer(@RequestParam(value = "gId") Integer gid, Integer type, double money, HttpServletRequest request){
+        HttpSession session=request.getSession();
+        Users user=(Users)session.getAttribute("user");
+        Integer uid = user.getUid();
+        double capital = user.getCapital();
+        double balance = user.getBalance();
+        if(service.zhifumoney(uid,gid,type,money,capital,balance)){
+            System.out.println("xxxxxx");
+            return "<h>ok</h>";
+        }else{
+            return "<h>no</h>";
+        }
     }
 
 }
